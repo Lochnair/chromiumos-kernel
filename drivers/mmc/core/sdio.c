5a83d5eb94043769eb554ecedb310983053ed450
Adrian Hunter <adrian.hunter@intel.com>
2017-06-09 15:27:18 +0300

    UPSTREAM: mmc: sdio: Keep card runtime resumed while adding function devices
    Drivers core will runtime suspend a device with no driver. That means the
    SDIO card will be runtime suspended as soon as it is added. It is then
    runtime resumed to add each function. That is entirely pointless, so add
    pm runtime get/put to keep the SDIO card runtime resumed until the function
    devices have been added.
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    (cherry picked from commit 4760257cb54e29e9a90e0ce849f4efcd67718816)
    
    BUG=b:140272233
    TEST=watch video on elm using wifi connection
    
    Signed-off-by: Matthias Kaehlcke <mka@chromium.org>
    Change-Id: Iee37a1bdacde131991f64a3742a3fbd75a2eb8fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2089414
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Brian Norris <briannorris@chromium.org>

--- drivers/mmc/core/sdio.c
+++ drivers/mmc/core/sdio.c
@@ -1120,6 +1120,12 @@ int mmc_attach_sdio(struct mmc_host *host)
 	 */
 	if (host->caps & MMC_CAP_POWER_OFF_CARD) {
 		/*
+		 * Do not allow runtime suspend until after SDIO function
+		 * devices are added.
+		 */
+		pm_runtime_get_noresume(&card->dev);
+
+		/*
 		 * Let runtime PM core know our card is active
 		 */
 		err = pm_runtime_set_active(&card->dev);
@@ -1193,6 +1199,9 @@ int mmc_attach_sdio(struct mmc_host *host)
 			goto remove_added;
 	}
 
+	if (host->caps & MMC_CAP_POWER_OFF_CARD)
+		pm_runtime_put(&card->dev);
+
 	mmc_claim_host(host);
 	return 0;
 
@@ -1202,7 +1211,9 @@ remove:
 remove_added:
 	/*
 	 * The devices are being deleted so it is not necessary to disable
-	 * runtime PM.
+	 * runtime PM. Similarly we also don't pm_runtime_put() the SDIO card
+	 * because it needs to be active to remove any function devices that
+	 * were probed, and after that it gets deleted.
 	 */
 	mmc_sdio_remove(host);
 	mmc_claim_host(host);
